ccd2e271eb84bf62c4bcadb9ebf5da899593c389
mikon <37613420+qluke@users.noreply.github.com>
2025-05-08 10:53:05 +0800

    ImageZoom

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,12 +1,14 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import { Mermaid } from '@/components/mdx/mermaid';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import { Mermaid } from "@/components/mdx/mermaid";
+import type { MDXComponents } from "mdx/types";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     Mermaid,
+    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
